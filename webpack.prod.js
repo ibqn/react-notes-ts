407f38a14bcd9dec221c641743b3880395d6356c
Evgeny Bobkin <evgeny.bobkin@gmail.com>
2017-10-06 12:29:54 +0200

    configure react for production

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,10 +1,15 @@
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const webpack = require('webpack');
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 
 
 module.exports = merge(common, {
   plugins: [
-    new UglifyJSPlugin(),
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin(),
   ],
 });
